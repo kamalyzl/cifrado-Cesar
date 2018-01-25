77e8469bb821566bf3d976f846e35922dbdf0dd4
kamalyzl <kamaly.kazal@gmail.com>
2018-01-25 17:06:28 -0500

    Añadiendo estilo

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Proceso Cifrado César con ES6
  
 ## Descripcion:
-
-Obtener una frase o texto ingresado por el usuario y devuelver el mismo mensaje, pero encriptado según el algoritmo de Cifrado César con el parámetro de desplazamiento de 33 espacios hacia la derecha
+Obtener una frase o texto ingresado por el usuario y devuelver el mismo mensaje, pero encriptado según el algoritmo de Cifrado César con la cantidad de desplazamiento a elección del usuario.
 
 _Por ejemplo:_
 
 * Texto original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
 * Texto codificado: HIJKLMNOPQRSTUVWXYZABCDEFG
- 
+
 [CLICK AQUI](https://kamalyzl.github.io/cifrado-Cesar/ "gh-pages")
+![Con titulo](assets/images/readme.png "titulo")
+ 
 
-
+_Autora:_ Kamaly Zapana Lorenzo
--- js/app.js
+++ js/app.js
@@ -2,19 +2,23 @@
 let cipherBtn = document.getElementById('cipher'),
   desCipherBtn = document.getElementById('deCipher'),
   input = document.getElementById('inputText'),
-  show = document.getElementById('show');
+  show = document.getElementById('show'),
+  number = document.getElementById('key');
 
 cipherBtn.addEventListener('click', cipher);
 function cipher() {
   let newTxt = '';
   let inpuTxt = input.value;
+  let key = parseInt(number.value);
   for (i = 0; i < inpuTxt.length; i++) {
     let toAscii = inpuTxt.charCodeAt(i);
-    if (toAscii >= 65 && toAscii <= 90)
-      newTxt += String.fromCharCode((toAscii - 65 + 33) % 26 + 65); // Mayus
-    else if (toAscii >= 97 && toAscii <= 122)
-      newTxt += String.fromCharCode((toAscii - 97 + 33) % 26 + 97); // Minus
-    else
+    if (toAscii >= 65 && toAscii <= 90) {
+      newTxt += String.fromCharCode((toAscii - 65 + key) % 26 + 65); // Mayus
+      desCipherBtn.className = 'visible';
+    } else if (toAscii >= 97 && toAscii <= 122) {
+      newTxt += String.fromCharCode((toAscii - 97 + key) % 26 + 97); // Minus
+      desCipherBtn.className = 'visible';
+    } else
       show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
   }
   show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
@@ -25,12 +29,13 @@ desCipherBtn.addEventListener('click', desCipher);
 function desCipher() {
   let newPhrase = '';
   let inpuTxt = input.value;
+  let key = parseInt(number.value);
   for (i = 0; i < inpuTxt.length; i++) {
     let toAscii = inpuTxt.charCodeAt(i);
     if (toAscii >= 65 && toAscii <= 90)
-      newPhrase += String.fromCharCode((toAscii - 65 - 33 + 26 * 2) % 26 + 65);
+      newPhrase += String.fromCharCode((toAscii - 65 - key + 26 * 2) % 26 + 65);
     else if (toAscii >= 97 && toAscii <= 122)
-      newPhrase += String.fromCharCode((toAscii - 97 - 33 + 26 * 2) % 26 + 97);
+      newPhrase += String.fromCharCode((toAscii - 97 - key + 26 * 2) % 26 + 97);
     else
       show.innerHTML = `<p> Resultado: ${newPhrase} </p>`;
   }
