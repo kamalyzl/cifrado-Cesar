5ffb14db7a0fd6a0028e91c747fca2e9c834c81d
kamalyzl <kamaly.kazal@gmail.com>
2018-01-25 16:57:03 -0500

    actualizando interaccion

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
 // trayendo variables DOM
-let cipherBtn = document.getElementById('cipher');
-let desCipherBtn = document.getElementById('deCipher');
-let input = document.getElementById('inputText');
-
+let cipherBtn = document.getElementById('cipher'),
+  desCipherBtn = document.getElementById('deCipher'),
+  input = document.getElementById('inputText'),
+  show = document.getElementById('show');
 
 cipherBtn.addEventListener('click', cipher);
 function cipher() {
-  let newPhrase = '';
-  let patronMayus = /^[A-Z\s]*$/;
-  let patronMinus = /^[a-z\s]*$/;
-  for (i = 0; i < input.value.length; i++) {
-    let toAscii = input.value.charCodeAt(i);
-    if (!input.value.search(patronMayus))
-      newPhrase += String.fromCharCode((toAscii - 65 + 33) % 26 + 65);
-    else if (!input.value.search(patronMinus))
-      newPhrase += String.fromCharCode((toAscii - 97 + 33) % 26 + 97);
+  let newTxt = '';
+  let inpuTxt = input.value;
+  for (i = 0; i < inpuTxt.length; i++) {
+    let toAscii = inpuTxt.charCodeAt(i);
+    if (toAscii >= 65 && toAscii <= 90)
+      newTxt += String.fromCharCode((toAscii - 65 + 33) % 26 + 65); // Mayus
+    else if (toAscii >= 97 && toAscii <= 122)
+      newTxt += String.fromCharCode((toAscii - 97 + 33) % 26 + 97); // Minus
     else
-      newPhrase += String.fromCharCode(toAscii);
+      show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
   }
-  alert(newPhrase);
+  show.innerHTML = `<p> Resultado: ${newTxt} </p>`;
 }
+
 
 desCipherBtn.addEventListener('click', desCipher);
 function desCipher() {
   let newPhrase = '';
-  let patronMayus = /^[A-Z\s]*$/;
-  let patronMinus = /^[a-z\s]*$/;
-  for (i = 0; i < input.value.length; i++) {
-    let toAscii = input.value.charCodeAt(i);
-    if (!input.value.search(patronMayus))
+  let inpuTxt = input.value;
+  for (i = 0; i < inpuTxt.length; i++) {
+    let toAscii = inpuTxt.charCodeAt(i);
+    if (toAscii >= 65 && toAscii <= 90)
       newPhrase += String.fromCharCode((toAscii - 65 - 33 + 26 * 2) % 26 + 65);
-    else if (!input.value.search(patronMinus))
+    else if (toAscii >= 97 && toAscii <= 122)
       newPhrase += String.fromCharCode((toAscii - 97 - 33 + 26 * 2) % 26 + 97);
     else
-      newPhrase += String.fromCharCode(toAscii);
+      show.innerHTML = `<p> Resultado: ${newPhrase} </p>`;
   }
-  alert(newPhrase);
+  show.innerHTML = `<p> Resultado: ${newPhrase} </p>`;
 }
